1f0cebb5218483ee097910cf0e45c84889da4feb
BrendonHawkins <artbitcodedata@gmail.com>
2021-08-20 11:34:40 -0400

    Moving along

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from 'axios';
+
+//////////importing//////
+import Form from './Components/Form'
+import FormDetails from './Components/FormDetails'
+import Home from './Components/Home'
+
+////////Set Initial Values///////
+
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+    <div>
+      <h2>Lambda Eats</h2>
+    </div>
     </>
   );
 };
---
+++ src/Components/Home.js
@@ -0,0 +1,18 @@
+import react from 'react'
+
+
+export default function Homepage() {
+    const history = useHistory();
+  
+    const routeToOrder = () => {
+      console.log("going to order form");
+      history.push("/pizza");
+    };
+  
+    return (
+      <div>
+        <img className="home-image" src="../Assets/Pizza.jpg" alt="" />
+        <button onClick={routeToOrder}>Order Now!</button>
+      </div>
+    );
+  }
