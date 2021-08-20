f9d29aa1f410062bf9422802371c76ea3137b9c3
BrendonHawkins <artbitcodedata@gmail.com>
2021-08-20 17:47:38 -0400

    More Styling; working on other pages now

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import React, { useState, useEffect } from "react";
 import {NavLink, Route, Switch } from "react-router-dom";
-import Home from "./Components/Home";
 import styled from "styled-components";
+import Asset from './Assets/Logo.png'
 // import * as yup from "yup";
 // import schema from "./Validation/schema";
 // import axios from "axios";
+import Home from "./Components/Home";
+import Form from './Components/Form';
+import FormDetails from './Components/FormDetails'
+
 
 
 
@@ -14,7 +18,7 @@ export default function App() {
   return (
     <Container>
         <nav>
-        <h1 className='store-header'>Lambda EATS</h1>
+        <img src={Asset} className='store-header'/>
         <div className='nav-links'>
           <button className='home-button'>
           <NavLink to='/' className="link">
@@ -45,13 +49,12 @@ const Container = styled.div`
   nav{
     display: flex;
     width: 100%;
-    background-color: white;
-    padding-top: 20px;
-    color:black;
+    background-color: black;
     border: black 1px;
   }
   .store-header{
     width: 50%;
+    height: auto;
     text-align: bottom;
     display: flex;
     justify-content: center;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -36,10 +36,29 @@ export default function Home() {
             <img src='https://fansided.com/files/2012/10/mcjordan.jpg' className='deliv-img'/>
             <h5>McMichael Jordan</h5>
             <p>$ American Fast Food - Burgers</p>
-            <button>23-39 min</button>
-            <button>$5.99 Delivery Fee</button>
+            <p>23-39 min</p>
+            <p>$5.99 Delivery Fee</p>
+        </div>
+        <div className='food-opt'>
+            <img src='https://images-platform.99static.com//oHOdDseQfI62toEzzqwiqiPZqu4=/576x50:1149x623/fit-in/500x500/99designs-contests-attachments/58/58919/attachment_58919838' className='deliv-img'/>
+            <h5>Bitter Greens</h5>
+            <p>$$ Semi-Healthy - Salads & Juice</p>
+            <p>50-60 min</p>
+            <p>$9.99 Delivery Fee</p>
+        </div>
+        <div className='food-opt'>
+            <img src='https://i.pinimg.com/originals/fd/32/1a/fd321a3faceb17c0293b6cfba9a76af3.jpg' className='deliv-img'/>
+            <h5>Comet Dollars</h5>
+            <p>$ Cafe - Coffee</p>
+            <p>10-25 min</p>
+            <p>No Delivery Fee</p>
         </div>
       </div>
+      <div className='bttm-img'>
+            <img src='https://i2.wp.com/theverybesttop10.com/wp-content/uploads/2015/05/Top-10-Crazy-and-Unusual-Square-Foods-6-510x492.jpg?resize=600%2C579'className='deliv-img'/>
+            <img src='https://mariettasquaremarket.com/wp-content/uploads/2018/12/Pita-Mediterranean-4.jpg'className='deliv-img'/>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSzD1NZ80clUsog9E-vaBeJMPwqHmFo1M1HDA&usqp=CAU'className='deliv-img'/>
+        </div>
     </HomeContainer>
   )
 };
@@ -50,6 +69,7 @@ const HomeContainer = styled.div `
     margin: 0px;
     font-family: san-serif;
     text-align: center;
+    height: auto;
   }
   h1{
     position: absolute;
@@ -69,15 +89,32 @@ const HomeContainer = styled.div `
       padding-right: 20px;
   }
   .opt-header{
+      padding: 20px;
+      display: flex;
 
   }
   .food-sec{
+    width: 100%;
     display: flex;
     flex-direction: row;
-    height: 75vh;
+    height: auto;
+    justify-content: space-around;
+    border-bottom: 1px solid white;
+    padding-bottom: 10px;
   }
   .food-opt{
       display: flex;
       flex-direction: column;
+  }
+  .deliv-img{
+    width: 300px;
+    height: 300px;
+  }
+  .bttm-img{
+      padding-top: 20px;
+      display: flex;
+      flex-direction: row;
+      height: auto;
+      justify-content: space-evenly;
   }
   `
